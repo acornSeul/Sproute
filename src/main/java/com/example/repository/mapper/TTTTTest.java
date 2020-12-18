3a7e3a3d31d006673a27f0b1f8b7c0c59eb786b0
eunsoo <eunsoo@DESKTOP-QVICA9Q>
2020-12-18 17:28:58 +0900

    테스트임돠

---
+++ src/main/java/com/example/repository/mapper/TTTTTest.java
@@ -0,0 +1,10 @@
+package com.example.repository.mapper;
+
+public class TTTTTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("테스트");
+	}
+
+}
